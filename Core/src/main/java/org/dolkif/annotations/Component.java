d6675357f20421316021cf50b4c41230c4a609fb
onsystem <onsystem@proton.me>
2023-02-28 12:43:28 +0100

    added default scope pattern in application properties

--- Core/src/main/java/org/dolkif/annotations/Component.java
+++ Core/src/main/java/org/dolkif/annotations/Component.java
@@ -6,4 +6,6 @@ import java.lang.annotation.*;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Component{
+    org.dolkif.context.Bean.ScopePattern scope() default org.dolkif.context.Bean.ScopePattern.SINGLETON;
+    String nameBean() default "";
 }
--- Core/src/main/java/org/dolkif/utils/ApplicationProperties.java
+++ Core/src/main/java/org/dolkif/utils/ApplicationProperties.java
@@ -1,7 +1,10 @@
 package org.dolkif.utils;
 
+import org.dolkif.context.Bean;
+
 public class ApplicationProperties {
     public static final String APPLICATION_NAME = "Dolkif";
     public static final String LOG_SEPARATOR = "<- | ->";
+    public static final Bean.ScopePattern DEFAULT_SCOPE_PATTERN = Bean.ScopePattern.SINGLETON;
 
 }
